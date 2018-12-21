07650317d814044e726f5c3baa39c78d78c0c00f
Vladimir Svidersky <svidersky@gmail.com>
2018-12-21 06:49:42 -0800

    Day 21. Part 1

---
+++ day-21/day-21/input.swift
@@ -0,0 +1,43 @@
+//
+//  input.swift
+//  day-21
+//
+//  Created by Vladimir Svidersky on 12/20/18.
+//  Copyright © 2018 Vladimir Svidersky. All rights reserved.
+//
+
+import Foundation
+
+let puzzle = [
+    "seti 123 0 5",
+    "bani 5 456 5",
+    "eqri 5 72 5",
+    "addr 5 2 2",
+    "seti 0 0 2",
+    "seti 0 3 5",
+    "bori 5 65536 3",
+    "seti 9010242 6 5",
+    "bani 3 255 1",
+    "addr 5 1 5",
+    "bani 5 16777215 5",
+    "muli 5 65899 5",
+    "bani 5 16777215 5",
+    "gtir 256 3 1",
+    "addr 1 2 2",
+    "addi 2 1 2",
+    "seti 27 6 2",
+    "seti 0 8 1",
+    "addi 1 1 4",  // 18
+    "muli 4 256 4",// 19
+    "gtrr 4 3 4",
+    "addr 4 2 2",
+    "addi 2 1 2",
+    "seti 25 5 2", // 23
+    "addi 1 1 1",  // 24
+    "seti 17 7 2", // 25
+    "setr 1 3 3",  // 26
+    "seti 7 2 2",  // 27
+    "eqrr 5 0 1",  // 28
+    "addr 1 2 2",
+    "seti 5 2 2"
+]
